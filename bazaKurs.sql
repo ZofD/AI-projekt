bdb8a76ef1e8e20a0e714822995a3e2fab7d8502
KamDziok <48258979+KamDziok@users.noreply.github.com>
2019-05-20 18:33:33 +0200

    Add files via upload

--- bazaKurs.sql
+++ bazaKurs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Czas generowania: 11 Maj 2019, 15:20
+-- Czas generowania: 20 Maj 2019, 18:28
 -- Wersja serwera: 10.1.16-MariaDB
 -- Wersja PHP: 5.6.24
 
@@ -36,8 +36,15 @@ CREATE TABLE `pytania` (
   `odp_druga` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `odp_trzecia` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
   `odp_czwarta` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-  `poprawna odpowiedz` int(11) NOT NULL
+  `poprawna_odpowiedz` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `pytania`
+--
+
+INSERT INTO `pytania` (`id_pytania`, `id_testu`, `pytanie`, `odp_pierwsza`, `odp_druga`, `odp_trzecia`, `odp_czwarta`, `poprawna_odpowiedz`) VALUES
+(1, 1, 'Pytanie', 'Odp1', 'odp2', 'odp3', 'odp4', '0');
 
 -- --------------------------------------------------------
 
@@ -47,8 +54,16 @@ CREATE TABLE `pytania` (
 
 CREATE TABLE `testy` (
   `id_testu` int(11) NOT NULL,
-  `nazwa_testu` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `nazwa_testu` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `testy`
+--
+
+INSERT INTO `testy` (`id_testu`, `nazwa_testu`) VALUES
+(1, 'Present continuous'),
+(2, 'Present simple');
 
 -- --------------------------------------------------------
 
@@ -60,7 +75,7 @@ CREATE TABLE `testy_uzytkownikow` (
   `id_testu` int(11) NOT NULL,
   `id_uzytkownika` int(11) NOT NULL,
   `zdany` tinyint(1) NOT NULL,
-  `ilosc_podejsc` int(11) NOT NULL
+  `data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -72,7 +87,7 @@ CREATE TABLE `testy_uzytkownikow` (
 CREATE TABLE `uzytkownicy` (
   `id_user` int(11) NOT NULL,
   `login` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `haslo` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `haslo` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `PESEL` varchar(11) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `miejscowosc` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -84,6 +99,13 @@ CREATE TABLE `uzytkownicy` (
   `imie` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `nazwisko` varchar(40) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id_user`, `login`, `haslo`, `email`, `PESEL`, `miejscowosc`, `ulica`, `nr_budynku`, `nr_lokalu`, `kod_pocztowy`, `poczta`, `imie`, `nazwisko`) VALUES
+(1, 'JanK00', '$2y$10$smMb3ZiuZeqx.tITf6HU.OIoK8ICq98n4s.Bbu0diXuqXRThxD2qu', 'jankowalski@gmail.com', '00000011111', 'Radom', 'ul. Radomska', '0', '', '00-000', 'Radom', 'Jan Kowalski', 'Kowalski'),
 
 --
 -- Indeksy dla zrzutów tabel
@@ -115,17 +137,17 @@ ALTER TABLE `uzytkownicy`
 -- AUTO_INCREMENT dla tabeli `pytania`
 --
 ALTER TABLE `pytania`
-  MODIFY `id_pytania` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pytania` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT dla tabeli `testy`
 --
 ALTER TABLE `testy`
-  MODIFY `id_testu` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_testu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT dla tabeli `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- uzytkownictKurs.sql
+++ uzytkownictKurs.sql
@@ -1,12 +1,27 @@
-# Uprawnienia dla `kursLogin`@`%`
+# Uprawnienia dla `kursLogin`@`localhost`
 
-GRANT USAGE ON *.* TO 'kursLogin'@'%' IDENTIFIED BY PASSWORD '*4E1FEECCEE8B3726B3FFF6D6F5687C7BA3C887F3';
+GRANT USAGE ON *.* TO 'kursLogin'@'localhost';
 
-GRANT SELECT (login, haslo) ON `projektkurs`.`uzytkownicy` TO 'kursLogin'@'%';
+GRANT SELECT ON `projektkurs`.* TO 'kursLogin'@'localhost';
+
+GRANT SELECT (haslo, id_user, login) ON `projektkurs`.`uzytkownicy` TO 'kursLogin'@'localhost';
 
 
-# Uprawnienia dla `kursRegi`@`%`
+# Uprawnienia dla `kursRegi`@`localhost`
 
-GRANT USAGE ON *.* TO 'kursRegi'@'%' IDENTIFIED BY PASSWORD '*4E1FEECCEE8B3726B3FFF6D6F5687C7BA3C887F3';
+GRANT USAGE ON *.* TO 'kursRegi'@'localhost' IDENTIFIED BY PASSWORD '*4E1FEECCEE8B3726B3FFF6D6F5687C7BA3C887F3';
 
-GRANT SELECT (PESEL, email, login), INSERT (imie, PESEL, poczta, email, ulica, haslo, kod_pocztowy, login, nr_budynku, miejscowosc, nazwisko, nr_lokalu) ON `projektkurs`.`uzytkownicy` TO 'kursRegi'@'%';
+GRANT SELECT (login, email), INSERT ON `projektkurs`.`uzytkownicy` TO 'kursRegi'@'localhost';
+
+
+# Uprawnienia dla `kursUser`@`localhost`
+
+GRANT USAGE ON *.* TO 'kursUser'@'localhost' IDENTIFIED BY PASSWORD '*4E1FEECCEE8B3726B3FFF6D6F5687C7BA3C887F3';
+
+GRANT SELECT, INSERT ON `projektkurs`.`testy_uzytkownikow` TO 'kursUser'@'localhost';
+
+GRANT SELECT ON `projektkurs`.`testy` TO 'kursUser'@'localhost';
+
+GRANT SELECT ON `projektkurs`.`pytania` TO 'kursUser'@'localhost';
+
+GRANT SELECT ON `projektkurs`.`uzytkownicy` TO 'kursUser'@'localhost';
